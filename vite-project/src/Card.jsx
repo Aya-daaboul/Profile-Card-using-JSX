51520ddee6bcf788710c5d4b7b7085c646d5bd8e
Aya <ayadaaboul02@outlook.com>
2025-01-08 20:29:54 +0200

    updated card

--- vite-project/src/Card.jsx
+++ vite-project/src/Card.jsx
@@ -2,9 +2,9 @@ import aya from "./assets/aya.jpg";
 import "./Card.css";
 function Card(){
     return(
-        <div className="Card">
-            <img src={aya} className="roundpics" alt="aya"></img>
-            <h3 className="username">Aya Fa</h3>
+        <div className="CardContainer">
+            <img src={aya} className="roundpics" alt="aya" width="200px" height="200px"></img>
+            <h3 className="username">Aya Daaboul</h3>
             <p className="card_desc">Junior DS and FSW</p>
         </div>
     )
